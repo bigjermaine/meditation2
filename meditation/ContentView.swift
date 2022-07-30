dbde8bf54d4ad67f08626b1a394929243c6c8d84
daniel jermaine <danieljermaine2@gmail.com>
2022-07-30 17:28:39 +0100

    Initial Commit

---
+++ meditation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  meditation
+//
+//  Created by Apple on 30/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ meditation/meditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditationApp.swift
+//  meditation
+//
+//  Created by Apple on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct meditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
